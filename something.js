a4878c6bc0c4e4108d091b30b79acb0e7cb2cc89
oskosk <oskosk@gmail.com>
2018-12-17 09:46:43 -0300

    Refactor

--- something.js
+++ something.js
@@ -1,7 +1,16 @@
 (function( el, render ) {
+	// Wait until container is already rendered
 	document.addEventListener('DOMContentLoaded', main, false);
+	
+	// Entrypoint for the whole code
 	function main() {
-		var b = document.querySelector('#wpbody');
-		render( el( function() { return el('div', {}, 'ochoa' ) } ), b);
+		// Get the container element
+		var container = document.querySelector('#wpbody');
+		// Render it with ReactDOM.render / wp.element.render
+		render( el( MainComponent ), container);
+	}
+
+	function MainComponent() {
+		return el( 'div', {}, 'Le content' );
 	}
 } )( wp.element.createElement, wp.element.render );
